e1fd168bed4f6a7c4baf63f201dcf85b2582716c
Nsuliman <nsuliman88@gmail.com>
2020-05-18 16:17:11 +0300

    classes refactoing and tests are done

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+
+const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
+
+let parsedInput = new Input(process.argv.slice(2));
+let notes = new Notes(parsedInput);
+
+console.log('up and running');

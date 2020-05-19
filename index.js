015e44ccce447d62f6f41ff20ad2f77e5b5bb23c
Nsuliman <nsuliman88@gmail.com>
2020-05-19 13:00:26 +0300

    lab 3 almost done

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
-const Input = require('./lib/input.js');
+const mongoose = require('mongoose');
+const dbURL = 'mongodb://localhost:27017/newNotes';
 const Notes = require('./lib/notes.js');
+const Input = require('./lib/input.js')
+// const Catagories = require('./models/catagories-schema.js');
 
-let parsedInput = new Input(process.argv.slice(2));
-let notes = new Notes(parsedInput);
+ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true});
 
-console.log('up and running');
+let args = process.argv.slice(2);
+
+let myInput = new Input(args);
+let myHandler = new Notes(myInput.command);
+
+
+
+

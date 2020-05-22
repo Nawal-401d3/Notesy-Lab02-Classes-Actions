1c23ba84e45dc6dc5e4bc4cf2188d0d072ca9f54
Nsuliman <nsuliman88@gmail.com>
2020-05-22 03:52:48 +0300

    lab 03 Done almost

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -9,12 +9,12 @@ const goodInput = ['-a', 'text'];
 
 describe('add() outputs console log', () => {
   it('handles good input for console log', () => {
-    // let result = new Input(goodInput);
-    // let newNotes = new Notes(result);
-    // let creeper = jest.spyOn(global.console, 'log');
-    // let add = jest.fn(new Notes.add());
-    // add();
-    // expect(creeper).toHaveBeenCalled();
+    let result = new Input(goodInput);
+    let newNotes = new Notes(result);
+    let creeper = jest.spyOn(global.console, 'log');
+    let add = jest.fn(new Notes.add());
+    add();
+    expect(creeper).toHaveBeenCalled();
   })
 })
 
